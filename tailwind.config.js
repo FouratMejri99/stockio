1de6a4c78fc4516a86513cdbdfe1f5863d948a27
Fourat Mejri <32815591+fouratMejri@users.noreply.github.com>
2025-02-09 06:14:23 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure Tailwind scans your components
+    "./public/index.html",
+  ],
   theme: {
-    extend: {
-      animation: {
-        orbit: "orbit calc(var(--duration)*1s) linear infinite",
-      },
-      keyframes: {
-        orbit: {
-          "0%": {
-            transform:
-              "rotate(calc(var(--angle) * 1deg)) translateY(calc(var(--radius) * 1px)) rotate(calc(var(--angle) * -1deg))",
-          },
-          "100%": {
-            transform:
-              "rotate(calc(var(--angle) * 1deg + 360deg)) translateY(calc(var(--radius) * 1px)) rotate(calc((var(--angle) * -1deg) - 360deg))",
-          },
-        },
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
